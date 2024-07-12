3ee2fd06cdac8b807dde488ec1bfdd3e188e0778
Hemant Nasa <hemantnasa777@gmail.com>
2024-07-12 13:43:00 +0530

    Added FileInputHandler for the MediaPlayer

--- src/script.js
+++ src/script.js
@@ -6,3 +6,19 @@ const speedDownElement = document.getElementById("speed-down");
 const volumeUpElement = document.getElementById("volume-up");
 const volumeDownElement = document.getElementById("volume-down");
 const toastMessage = document.getElementById("toast");
+
+const inputHandler = (obj) => {
+    openVideo.click();
+}
+openVideoBtn.addEventListener("click", inputHandler);
+
+openVideo.addEventListener("change", (obj) => {
+    console.log(obj);
+    const video = obj.target.files[0]; 
+    const link = URL.createObjectURL(video);
+    const videoElement = document.createElement("video");
+    videoElement.src = link;
+    videoElement.play();
+    videoElement.setAttribute("id", "video")
+    mainElement.appendChild(videoElement);
+})
